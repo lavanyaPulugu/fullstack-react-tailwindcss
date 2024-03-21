2c27013877c9a54e3c3a48a40392558d1fa87a0c
lavanyapulugu <lavanya415@gmail.com>
2024-03-21 19:30:42 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/User.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,9 +16,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
