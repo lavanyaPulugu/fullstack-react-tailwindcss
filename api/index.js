fcf57a192b94a4f0d6f35a79716c83cfa734ebfd
lavanyapulugu <lavanya415@gmail.com>
2024-03-21 21:47:34 +0530

    add middleware and functions to handle errors

--- api/index.js
+++ api/index.js
@@ -24,3 +24,14 @@ app.listen(5000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// create middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
