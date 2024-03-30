422a2a3f1eadc580cc8df3fb0ffe6a5dae03a21d
lavanyapulugu <lavanya415@gmail.com>
2024-03-30 10:10:21 +0530

    update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/User.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(5000, () => {
   console.log("server is running on port 5000");
