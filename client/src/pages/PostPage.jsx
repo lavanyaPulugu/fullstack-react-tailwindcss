9504f41753d71657b382610cc6b112341bd24f9a
lavanyapulugu <lavanya415@gmail.com>
2024-04-03 16:53:13 +0530

    add call to action page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Spinner } from "flowbite-react";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import CallToAction from "../components/CallToAction";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -67,6 +68,9 @@ const PostPage = () => {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
+      </div>
     </main>
   );
 };
